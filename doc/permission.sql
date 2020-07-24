459fe6fd13845963727ff33f09b56587c688de0d
joker_dj <2332810801@qq.com>
2020-07-24 17:43:53 +0800

    文件上传sql

--- doc/permission.sql
+++ doc/permission.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 23/07/2020 18:03:21
+ Date: 24/07/2020 17:42:47
 */
 
 SET NAMES utf8mb4;
@@ -58,15 +58,14 @@ CREATE TABLE `employee`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `dep_id`(`dep_id`) USING BTREE,
   CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`dep_id`) REFERENCES `department` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of employee
 -- ----------------------------
-INSERT INTO `employee` VALUES (7, 'system', '123', NULL, '123', '123456@qq.com', '2020-07-02 00:00:00', 0, 1, 1);
-INSERT INTO `employee` VALUES (8, 'admin', '123', NULL, '123456', '233@qq.com', '2020-07-15 00:00:00', 1, 1, 1);
-INSERT INTO `employee` VALUES (9, 'use', '123', NULL, '1235', '123', '2020-07-15 00:00:00', 1, 1, 1);
-INSERT INTO `employee` VALUES (16, '1943001450277', '123', NULL, '123', '123', '2020-07-23 00:00:00', 1, 1, 1);
+INSERT INTO `employee` VALUES (17, 'system', 'be600598bc05b3d0b097f2d397f366e5', NULL, '123', '123456@qq.com', '2020-07-24 00:00:00', 1, 1, 1);
+INSERT INTO `employee` VALUES (18, 'admin', '2fe79843293c57e17c0e070d28322bdc', NULL, '123', '123456@qq.com', '2020-07-24 00:00:00', 1, 1, 1);
+INSERT INTO `employee` VALUES (19, 'use', '2fe79843293c57e17c0e070d28322bdc', NULL, '11111', '123456@qq.com', '2020-07-24 00:00:00', 1, 0, 1);
 
 -- ----------------------------
 -- Table structure for employee_role_rel
@@ -84,7 +83,37 @@ CREATE TABLE `employee_role_rel`  (
 -- ----------------------------
 -- Records of employee_role_rel
 -- ----------------------------
-INSERT INTO `employee_role_rel` VALUES (16, 2);
+INSERT INTO `employee_role_rel` VALUES (18, 2);
+INSERT INTO `employee_role_rel` VALUES (17, 5);
+INSERT INTO `employee_role_rel` VALUES (18, 5);
+INSERT INTO `employee_role_rel` VALUES (18, 7);
+INSERT INTO `employee_role_rel` VALUES (19, 7);
+
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `text` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `url` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `parent_id` bigint(20) NULL DEFAULT NULL,
+  `permission_id` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `parent_id`(`parent_id`) USING BTREE,
+  INDEX `permisssion_id`(`permission_id`) USING BTREE,
+  CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `menu` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `menu_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`pid`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of menu
+-- ----------------------------
+INSERT INTO `menu` VALUES (1, '系统管理', '', NULL, 8);
+INSERT INTO `menu` VALUES (2, '员工管理', '/employee', 1, 4);
+INSERT INTO `menu` VALUES (3, '权限管理', '/role', 1, 6);
+INSERT INTO `menu` VALUES (4, '菜单管理', '/menu', 1, 5);
+INSERT INTO `menu` VALUES (8, '部门管理', '/departmentList', 1, 7);
 
 -- ----------------------------
 -- Table structure for permission
@@ -103,7 +132,11 @@ CREATE TABLE `permission`  (
 INSERT INTO `permission` VALUES (1, '员工添加', 'employee:add');
 INSERT INTO `permission` VALUES (2, '员工删除', 'employee:delete');
 INSERT INTO `permission` VALUES (3, '员工编辑', 'employee:edit');
-INSERT INTO `permission` VALUES (4, '员工主页', 'employee:index');
+INSERT INTO `permission` VALUES (4, '员工管理', 'employee:index');
+INSERT INTO `permission` VALUES (5, '菜单管理', 'menu:index');
+INSERT INTO `permission` VALUES (6, '权限管理', 'permission:index');
+INSERT INTO `permission` VALUES (7, '部门管理', 'department:index');
+INSERT INTO `permission` VALUES (8, '系统管理', 'system:index');
 
 -- ----------------------------
 -- Table structure for role
@@ -140,11 +173,20 @@ INSERT INTO `role_permission_rel` VALUES (2, 1);
 INSERT INTO `role_permission_rel` VALUES (2, 2);
 INSERT INTO `role_permission_rel` VALUES (2, 3);
 INSERT INTO `role_permission_rel` VALUES (2, 4);
-INSERT INTO `role_permission_rel` VALUES (5, 1);
-INSERT INTO `role_permission_rel` VALUES (5, 2);
-INSERT INTO `role_permission_rel` VALUES (5, 3);
-INSERT INTO `role_permission_rel` VALUES (5, 4);
-INSERT INTO `role_permission_rel` VALUES (7, 1);
-INSERT INTO `role_permission_rel` VALUES (7, 2);
+INSERT INTO `role_permission_rel` VALUES (7, 4);
+INSERT INTO `role_permission_rel` VALUES (7, 8);
+
+-- ----------------------------
+-- Table structure for systemlog
+-- ----------------------------
+DROP TABLE IF EXISTS `systemlog`;
+CREATE TABLE `systemlog`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `optime` datetime(0) NULL DEFAULT NULL,
+  `ip` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `function` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `params` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
